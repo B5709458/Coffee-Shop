a9e72fcd1920e8a9dbcc1015758dd2a35260860f
B5709458 <hatai.wiriya@gmail.com>
2018-07-06 16:29:49 +0700

    Edit Project

--- Project/src/test/java/com/springsource/roo/coffeeshop/domain/EmployeeDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package com.springsource.roo.coffeeshop.domain;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Employee.class)
-public class EmployeeDataOnDemand {
-}
--- Project/src/test/java/com/springsource/roo/coffeeshop/domain/MenuDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package com.springsource.roo.coffeeshop.domain;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Menu.class)
-public class MenuDataOnDemand {
-}
